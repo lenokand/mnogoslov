93590a3ae3244e7aa92fcf778365d3c1f077feb1
lenokand <shtanko1991@gmail.com>
2021-12-29 22:00:24 +0300

    seo-page

--- src/app.js
+++ src/app.js
@@ -1,39 +1,61 @@
-// // адаптив
-// function fontSize() {
-//     if ($(window).width() < 900) {
-//       $('html').css({
-//         fontSize: 16 + 'px'
-//       });
-//     }
-  
-//     if ($(window).width() > 900) {
-//       var width = 1920; // ширина, от которой идет отсчет
-  
-//       var fontSize = 16; // минимальный размер шрифта
-  
-//       var bodyWidth = $('html').width();
-//       var multiplier = bodyWidth / width;
-//       if ($('html').width() >= width) fontSize = Math.floor(fontSize * multiplier);
-//       if ($('html').width() < width) fontSize = Math.floor(fontSize * multiplier);
-//       $('html').css({
-//         fontSize: fontSize + 'px'
-//       });
-//     }
-//   }
-  
-//   $(function () {
-//     fontSize();
-//   });
-//   $(window).resize(function () {
-//     fontSize();
-//   });
+// import Swiper JS
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
+
+// configure Swiper to use modules
+Swiper.use([Navigation, Pagination]);
 
 
 
 const icon = document.querySelector('.burger-icon');
-
+let menu = document.querySelector('.menu_block')
+let body = document.body
   icon.addEventListener('click', (event) => {
     icon.classList.toggle("open");
+    menu.classList.toggle("active");
+    body.classList.toggle('menu_open')
+
+
  
 });
+
+
+let submenuBtn = document.querySelectorAll('.menu .open_submenu')
+
+submenuBtn.forEach((item, index) => {
+  item.addEventListener('click', function(){
+
+    let submenu = document.querySelectorAll('.menu .submenu')
+
+    
+    if (item.classList.contains('active')){
+      item.classList.remove('active')
+      submenu[index].classList.remove('show')
+
+
+    } else {
+
+      let ac_menuBtn = document.querySelector('.menu .open_submenu.active')
+
+    
+
+      if (ac_menuBtn) {
+        ac_menuBtn.classList.remove('active')
+        let ac_submenu = document.querySelector('.menu .submenu.show')
+        ac_submenu.classList.remove('show')
+      }
+  
+  
+      item.classList.add('active')
+      submenu[index].classList.add('show')
+    }
+
+  })
+
+})
+
+
+var seo_main = document.getElementById('seo_main');
+var parallaxInstance = new Parallax(seo_main);
