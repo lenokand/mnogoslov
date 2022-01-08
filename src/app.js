84f45c42363b7514ec81c150d17dd145131711cd
lenokand <shtanko1991@gmail.com>
2022-01-09 00:26:40 +0300

    quiz

--- src/app.js
+++ src/app.js
@@ -59,3 +59,105 @@ submenuBtn.forEach((item, index) => {
 
 var seo_main = document.getElementById('seo_main');
 var parallaxInstance = new Parallax(seo_main);
+
+
+
+// квиз
+
+let changeScreen = document.querySelector("#change_screen") //кнопка переключения слайдов
+let arrSlides = document.querySelectorAll('.question_screen') //выборка слайдов
+let currentSlide = 0
+let submitBtn = document.querySelector('#quiz_submit') //кнопка отправки формы
+
+let currentQuestion = document.querySelector('.current_question') // передача номера текущего слайда
+let coloredBlock = document.querySelectorAll('.prof_item') // закрашивание блоков
+
+if (arrSlides.length > 0) {
+
+
+
+  // изначальная блокировка кнопки
+  changeScreen.setAttribute('disabled', 'disabled')
+
+  unDisabledBtn(arrSlides[0])
+
+  // функция для блока кнопки
+  function unDisabledBtn(item) {
+
+
+    let listOfInput = item.querySelectorAll('input')
+    listOfInput.forEach((item, index) => {
+
+      item.addEventListener('change', function () {
+
+        changeScreen.removeAttribute('disabled')
+
+      })
+    })
+
+  }
+
+
+
+  changeScreen.addEventListener('click', function () {
+
+    if ((currentSlide + 2) !== arrSlides.length) {
+
+      changeScreen.setAttribute('disabled', 'disabled')
+
+
+
+
+      arrSlides[currentSlide].classList.remove('active')
+
+      currentSlide++
+
+      arrSlides[currentSlide].classList.add('active')
+
+      unDisabledBtn(arrSlides[currentSlide])
+
+      // coloredBlock[currentSlide].style.cssText = "background-image: url(img/profit_fon.png);"
+      coloredBlock[currentSlide].classList.add('colored')
+      currentQuestion.textContent = (currentSlide + 1)
+
+
+    } else {
+      arrSlides[currentSlide].classList.remove('active')
+
+      currentSlide++
+      // coloredBlock[currentSlide].style.cssText = "background-image: url(img/profit_fon.png);"
+      coloredBlock[currentSlide].classList.add('colored')
+      currentQuestion.textContent = (currentSlide + 1)
+      arrSlides[currentSlide].classList.add('active')
+
+      changeScreen.style.cssText = "display: none;"
+      currentSlide = 0
+    }
+
+
+  })
+
+
+  submitBtn.addEventListener('submit', function () {
+    currentSlide = 0
+
+    arrSlides[(arrSlides.length - 1)].classList.remove('active')
+
+    arrSlides[0].classList.add('active')
+    changeScreen.style.cssText = "display: block;"
+
+
+    coloredBlock.forEach((block, index) => {
+
+      if (index !== 0) {
+        block.classList.remove('active')
+        // block.style.cssText = "background-image: none;"
+      }
+
+
+    })
+
+  })
+
+
+}
